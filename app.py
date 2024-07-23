774f6da4bc7fe57ba7aa50aad4010aaf8d178101
Jiwoo <112547554+kjw3757@users.noreply.github.com>
2024-07-23 16:58:42 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.title("KBO 구단별 최적 라인업 추천")
+
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    if st.button('KIA'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-ldshjrywymhbvo5vjupqm2.streamlit.app/" />', unsafe_allow_html=True)
+
+with col2:
+    if st.button('KT'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-a9jbuhkipdzhk4k3owhcc8.streamlit.app/" />', unsafe_allow_html=True)
+
+with col3:
+    if st.button('LG'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-mqepjolisjmqtmuuwnh6g7.streamlit.app/" />', unsafe_allow_html=True)
+
+with col4:
+    if st.button('NC'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-pk8t8h4jlyajgv9byd8y7.streamlit.app/" />', unsafe_allow_html=True)
+
+with col5:
+    if st.button('SSG'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-4wvnrhmba3tze9wugnnsez.streamlit.app/" />', unsafe_allow_html=True)
+
+col6, col7, col8, col9, col10 = st.columns(5)
+
+with col6:
+    if st.button('두산'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-yuy2tpsvtmvp37azejuqke.streamlit.app/" />', unsafe_allow_html=True)
+
+with col7:
+    if st.button('한화'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-j3ez4gy6zwhvtkmviafodj.streamlit.app/" />', unsafe_allow_html=True)
+
+with col8:
+    if st.button('롯데'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-f9avyaas8anu8e4sbhfcgj.streamlit.app/" />', unsafe_allow_html=True)
+
+with col9:
+    if st.button('키움'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-zer4dgwitg4oiiv8cd7tsn.streamlit.app/" />', unsafe_allow_html=True)
+
+with col10:
+    if st.button('삼성'):
+        st.markdown('<meta http-equiv="refresh" content="0; url=https://baseballlineuppredictionver2-wjc9rgyeqmybwwmhfohaam.streamlit.app/" />', unsafe_allow_html=True)
